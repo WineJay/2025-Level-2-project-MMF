c5ff23b422f7e6d30be596e7d3bb86a5c6681eeb
WineJay <hossaina3745@masseyhigh.school.nz>
2025-02-26 09:58:54 +1300

    PANDA & PROFIT

--- B_01_MMF.v2.py
+++ B_01_MMF.v2.py
@@ -1,37 +1,17 @@
 import pandas
 
 
-# functions goes here
-def int_check(question):
-    """checks that the user enter an integer. """
+# Functions go here
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
 
-    error = "Oops - please enter an integer. "
-
-    while True:
-        try:
-            # Return the response if it's an integer
-            response = int(input(question))
-            return response
-
-        except ValueError:
-            print(error)
-
-
-def not_blank(question):
-    """Checks that a user response is not blank"""
-
-    while True:
-        response = input(question)
-
-        if response != "":
-            return response
-
-        print("Sorry, this can't be blank. Please try again.\n")
+    print(f"{decoration * 3} {statement} {decoration * 3}")
 
 
 def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
     """checks that the user enter th full word
-        or the first letter of a word from a list of valid response"""
+    or the first letter of a word from a list of valid response"""
 
     while True:
 
@@ -49,15 +29,67 @@ def string_check(question, valid_answers=('yes', 'no'), num_letters=1):
 
         print(f"Please choose an option from {valid_answers}")
 
+    # main routine goes here
+
+
+def instructions():
+    make_statement("Instructions", "ℹ️")
+    """Instructions for using MMF"""
+
+    print('''
+    
+For each ticket holder enter...
+- Their name
+- Their age
+- The payment method (cash / credit)
+
+The program will record the ticket-sale and calculate the ticket cost (and the profit).
+
+Once you have either sold all of the tickets or entered the exit code ('xxx'), the program will display the ticket sales
+information and write the data to a text file.
+ 
+it will also choose one lucky ticket holder wins the draw (their ticket is free).
+
+    ''')
+
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again.\n")
+
+
+def int_check(question):
+    """checks that the user enter an integer. """
+
+    error = "Oops - please enter an integer. "
+
+    while True:
+        try:
+            # Return the response if it's an integer
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print(error)
+
 
 def currency(x):
     """formats number as currency ($#,##)"""
     return "${:.2f}".format(x)
 
-    # main routine goes here
 
+# Main Routine goes here
 
-# main routine starts here
+# initialise tickets numbers
+MAX_TICKETS = 5
+tickets_sold = 0
 
 # initialize variable / non-default options for string checker
 payment_ans = ['cash', 'credit']
@@ -67,8 +99,6 @@ CHILD_PRICE = 7.50
 ADULT_PRICE = 10.50
 SENIOR_PRICE = 6.50
 
-# tickets total sold
-tickets_sold = 0
 # Credit card surcharge (currently 5%)
 CREDIT_SURCHARGE = 0.05
 
@@ -83,21 +113,32 @@ mini_movie_dict = {
     'Surcharge': all_surcharge
 }
 
-# loop for testing purposes...
-while True:
-    print()
+# programme main heading
+make_statement("Mini-Movie Fundraiser Program", "🍿")
 
+# Ask user if they want to see the instructions and display them if necessary
+# Display them if necessary
+print()
+want_instructions = string_check("Do you want to see the instructions?")
+if want_instructions == "yes":
+    instructions()
+
+print()
+
+# loop to get name, age and payment details
+while tickets_sold < MAX_TICKETS:
+    print()
     # ask user for their name
     name = not_blank("Name: ")  # replace with call to 'not blank' function!
-    # if name is exit code, break out of loop
 
+    # if name is exit code, break out of loop
     if name == "xxx" and tickets_sold > 0:
         break
     elif name == "xxx":
-        print("You need to sell at least one ticket")
+        print("oops, you need to sell at least one ticket")
         continue
 
-    # ask for their age and check if it's between 12 and 120
+        # ask for their age and check if it's between 12 and 120
     age = int_check("Age: ")
 
     # output error message / success message
@@ -120,8 +161,6 @@ while True:
         print(f"{name} is too old")
         continue
 
-    tickets_sold += 1
-
     # ask users for their payment method (cash/ credit/ ca/ cr)
     pay_method = string_check("Payment method: ", payment_ans, 2)
 
@@ -136,6 +175,10 @@ while True:
     all_names.append(name)
     all_tickets_costs.append(ticket_price)
     all_surcharge.append(surcharge)
+
+    tickets_sold += 1
+
+# end of the ticket loop
 
 # create dataframe / table from dictionary
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
@@ -158,3 +201,8 @@ print(mini_movie_frame.to_string(index=False))
 print()
 print(f"Total Paid: ${total_paid:.2f}")
 print(f"Total Profit: ${total_profit:.2f}")
+
+if tickets_sold == MAX_TICKETS:
+    print(f"You have sold all the tickets (ie: {MAX_TICKETS} tickets")
+else:
+    print(f"You have sold {tickets_sold} / {MAX_TICKETS} tickets.")
